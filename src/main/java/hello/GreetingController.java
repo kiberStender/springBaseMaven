eba84b937ed1778439d9702ae3118d9d429ad0d3
Kleber Stender <kleber.stender@gmail.com>
2016-05-24 20:49:28 -0300

    Added json response method

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,15 +1,20 @@
 package hello;
 
-import org.springframework.stereotype.Controller;
+import model.Greeting;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by sirkleber on 24/05/16.
  */
-@Controller
+@RestController
 public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public String greeting(
@@ -17,5 +22,10 @@ public class GreetingController {
     ){
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @RequestMapping("/greetingJson")
+    public Greeting greetingJson(@RequestParam(value="name", defaultValue="World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
