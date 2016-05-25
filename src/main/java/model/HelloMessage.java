cdf07269d616cc55952dc79b82f32702f7f01b59
Kleber Stender <kleber.stender@gmail.com>
2016-05-25 10:13:17 -0300

    Created model message HelloMessage

---
+++ src/main/java/model/HelloMessage.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Created by sirkleber on 25/05/16.
+ */
+public class HelloMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
