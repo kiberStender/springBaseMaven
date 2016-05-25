945dfda39376742cb585c722ea454a14389caf0e
Kleber Stender <kleber.stender@gmail.com>
2016-05-25 13:24:28 -0300

    Splited controller in two from and moved them to controllers package. Added annotation in main class to search for the controllers when booting

--- src/main/java/controllers/GreetingControllerRest.java
+++ src/main/java/controllers/GreetingControllerRest.java
@@ -1,7 +1,6 @@
-package hello;
+package controllers;
 
 import model.Greeting;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,20 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * Created by sirkleber on 24/05/16.
+ * Created by sirkleber on 25/05/16.
  */
 @RestController
-public class GreetingController {
+public class GreetingControllerRest {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/greeting")
-    public String greeting(
-      @RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model
-    ){
-        model.addAttribute("name", name);
-        return "greeting";
-    }
 
     @RequestMapping("/greetingJson")
     public Greeting greetingJson(@RequestParam(value="name", defaultValue="World") String name){
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,8 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("controllers")
 public class HelloWorld {
   public static void main(String... args){
     SpringApplication.run(HelloWorld.class, args);
